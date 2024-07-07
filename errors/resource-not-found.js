050d0e144826819abfb2fa6d26f9458a35d55d46
Praise Izuagie <izuagiepraise@gmail.com>
2024-07-07 22:54:27 +0100

    Name error class correctly

--- errors/resource-not-found.js
+++ errors/resource-not-found.js
@@ -2,11 +2,11 @@ const { StatusCodes } = require("http-status-codes");
 
 const { BaseAPIError } = require("./base.js");
 
-class BadRequestError extends BaseAPIError {
+class ResourceNotFoundError extends BaseAPIError {
   constructor(message, status) {
     super(message, status || "Resource Not Found");
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
-module.exports = { BadRequestError };
+module.exports = { ResourceNotFoundError };
